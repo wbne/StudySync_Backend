bdb5e3f944e1753c25cb1ca744d4b262837b6da4
jamster02 <jaxsonmyrick@gmail.com>
2021-10-02 21:29:25 -0500

    shotgun fixes

--- routes/index.js
+++ routes/index.js
@@ -17,38 +17,45 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.render('index', { title: 'Express' });
 });
 
 rooms = [];
 
 router.param('room', function(req, res, next, id){
+  res.header("Access-Control-Allow-Origin", "*");
   req.room = rooms.find(room => room.id == id);
   next();
 });
 
 /* Returns the room at an index in json */
 router.get('/room/:room', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.json(req.room);
 });
 
 router.param('subject', function(req, res, next, subject){
+  res.header("Access-Control-Allow-Origin", "*");
   req.subject = subject;
   next();
 });
 
 /* Returns all rooms of a subject in json */
 router.get('/subject/:subject', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.json(rooms.filter(room => room.subject == req.subject));
 });
 
 router.param('increment', function(req, res, next, amount) {
+  res.header("Access-Control-Allow-Origin", "*");
   req.increment = Number(amount);
   next();
 });
 
 /* Increments the numMembers of a specific index by an amount */
 router.put('/room/:room/:increment', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   if (req.room.numMembers + req.increment <= req.room.maxRoomSize && req.room.numMembers + req.increment >= 0) {
     req.room.numMembers += req.increment;
     res.send('Success');
@@ -58,18 +65,21 @@ router.put('/room/:room/:increment', function(req, res, next) {
 });
 
 router.param('password', function(req, res, next, password) {
+  res.header("Access-Control-Allow-Origin", "*");
   req.password = password;
   next();
 });
 
 /* Adds a new room of a certain subject and password */
 router.put('/add/:subject/:password', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   rooms.push(new Room(rooms.length, req.subject, req.password));
   res.send('Success');
 });
 
 /* Increments the numMembers of a room in a subject and returns that room */
 router.put('/subject/:subject/:increment', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   targetRoom = rooms.filter(room => room.subject == req.subject && room.numMembers + req.increment <= room.maxRoomSize).sort(function(a,b){return b - a})[0]; // TODO use a more efficient sort
   if (targetRoom == undefined) {
     res.send('Failure');
@@ -82,6 +92,7 @@ router.put('/subject/:subject/:increment', function(req, res, next) {
 var users = {};
 
 router.param('user', function(req, res, next, email) {
+  res.header("Access-Control-Allow-Origin", "*");
   if (users[email]) {
     req.user = users[email];
   } else {
@@ -92,21 +103,25 @@ router.param('user', function(req, res, next, email) {
 
 /* Gets the specified subjects for the user */
 router.get('/user/:user', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.json(req.user);
 });
 
 router.param('email', function(req, res, next, email) {
+  res.header("Access-Control-Allow-Origin", "*");
   req.email = email;
   next();
 });
 
 router.param('subjects', function(req, res, next, subjects) {
+  res.header("Access-Control-Allow-Origin", "*");
   req.subjects = subjects.split(',');
   next();
 });
 
 /* Sets the user's subjects to a list of comma-separated subjects */
 router.put('/user/:email/:subjects', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   users[req.email] = req.subjects;
   res.send('Success');
 });
